984aee98d7d39bb28671b300a9556eb0d1e54cd8
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-03-04 11:43:49 +0900

    List 1.5, p.17

--- test/junit/tutorial/CalculatorTest.java
+++ test/junit/tutorial/CalculatorTest.java
@@ -9,10 +9,17 @@ public class CalculatorTest {
 	
 	// 日本語でTestメソッド名を書くと結果などが非常に見やすいため"推奨"されている．
 	@Test
-	public void multiplyで乗算結果が取得できる() {
+	public void multiplyで3と4の乗算結果が取得できる() {
 		Calculator calc = new Calculator();
 		int expected = 12;
 		int actual = calc.multiply(3, 4);
+		assertThat(actual, is(expected));
+	}
+	@Test
+	public void multiplyで5と7の乗算結果が取得できる() {
+		Calculator calc = new Calculator();
+		int expected = 12;
+		int actual = calc.multiply(5, 7);
 		assertThat(actual, is(expected));
 	}
 
