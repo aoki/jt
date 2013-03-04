f5d50edbb16d3e497c243f350ba653f4e88ccf60
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-03-04 12:01:26 +0900

    List 2.1, p.30

--- src/junit/tutorial/Calculator.java
+++ src/junit/tutorial/Calculator.java
@@ -1,6 +1,12 @@
 package junit.tutorial;
 
 public class Calculator {
+	/**
+	 * 引数で与えられた2つの値を掛けあわせた値を返す
+	 * @param x 1つ目の引数
+	 * @param y 2つ目の引数
+	 * @return xとyを掛けあわせた値
+	 */
 	public int multiply(int x, int y) {
 		return x * y;
 	}
