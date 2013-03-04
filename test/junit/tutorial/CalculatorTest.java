59a4c3fd98b98515889c2507589eb9740d4246fa
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-03-04 11:40:38 +0900

    List 1.4, p.15

--- test/junit/tutorial/CalculatorTest.java
+++ test/junit/tutorial/CalculatorTest.java
@@ -1,6 +1,7 @@
 package junit.tutorial;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
 
@@ -9,7 +10,10 @@ public class CalculatorTest {
 	// 日本語でTestメソッド名を書くと結果などが非常に見やすいため"推奨"されている．
 	@Test
 	public void multiplyで乗算結果が取得できる() {
-		fail("Not yet implemented");
+		Calculator calc = new Calculator();
+		int expected = 12;
+		int actual = calc.multiply(3, 4);
+		assertThat(actual, is(expected));
 	}
 
 }
