c8fea11e58cee97f0a7aeb09adca752b541cdac8
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-03-04 11:39:29 +0900

    List 1.3, p.13

--- test/junit/tutorial/CalculatorTest.java
+++ test/junit/tutorial/CalculatorTest.java
@@ -5,9 +5,10 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CalculatorTest {
-
+	
+	// 日本語でTestメソッド名を書くと結果などが非常に見やすいため"推奨"されている．
 	@Test
-	public void test() {
+	public void multiplyで乗算結果が取得できる() {
 		fail("Not yet implemented");
 	}
 
