50387d05bafb03c83b9b9cab4e45a74a6ae376eb
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-03-08 17:22:26 +0900

    List 4.5, p.74

--- test/junit/tutorial/IsDate.java
+++ test/junit/tutorial/IsDate.java
@@ -4,8 +4,13 @@ import java.util.Date;
 
 import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 
 public class IsDate extends BaseMatcher<Date> {
+
+	public static Matcher<Date> dateOf(int yyyy, int mm, int dd) {
+		return new IsDate();
+	}
 
 	@Override
 	public boolean matches(Object actual) {
