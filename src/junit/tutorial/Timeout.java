e98d0c3216ae44ff275ecfd702c129820936b9a9
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-03-08 11:23:06 +0900

    List 3.11, p.52

---
+++ src/junit/tutorial/Timeout.java
@@ -0,0 +1,17 @@
+package junit.tutorial;
+
+
+public class Timeout {
+
+	public Timeout() {}
+
+	public int doLongTask() {
+		try {
+			java.util.concurrent.TimeUnit.MILLISECONDS.sleep(10);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 100;
+	}
+}
---
+++ test/junit/tutorial/TimeoutTest.java
@@ -0,0 +1,17 @@
+package junit.tutorial;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+
+public class TimeoutTest {
+
+	@Test(timeout = 100L)
+	public void timeoutTest() throws Exception {
+		Timeout sut = new Timeout();
+		int actual = sut.doLongTask();
+		assertThat(actual, is(100));
+	}
+
+}
