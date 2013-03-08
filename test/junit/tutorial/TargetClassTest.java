303bbb21f47a960d4a2f95da0f3e3a805094cd5c
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-03-08 12:00:01 +0900

    List 3.18, p.57

--- test/junit/tutorial/TargetClassTest.java
+++ test/junit/tutorial/TargetClassTest.java
@@ -1,6 +1,12 @@
 package junit.tutorial;
 
+import org.junit.Test;
+
 //テスト対象クラス (TargetClass.java)
 public class TargetClassTest {
-
+	@Test(expected = IllegalArgumentException.class)
+	public void 例外テスト() {
+		TargetClass sut = new TargetClass();
+		sut.doSomething();
+	}
 }
