e5aa4edd42b46de25f309bcfce5b197d4b10dd5a
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-03-14 12:00:44 +0900

    List 5.9, p.90

---
+++ test/junit/tutorial/CategoriesTest.java
@@ -0,0 +1,12 @@
+package junit.tutorial;
+
+import org.junit.experimental.categories.Categories;
+import org.junit.experimental.categories.Categories.ExcludeCategory;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Categories.class)
+@ExcludeCategory(SlowTests.class)
+@SuiteClasses(SlowAndFastTest.class)
+public class CategoriesTest {
+}
