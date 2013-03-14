fa2f1b8347e6f654363b6421404732891b947119
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-03-14 12:00:24 +0900

    List 5.8, p.90

---
+++ test/junit/tutorial/SlowAndFastTest.java
@@ -0,0 +1,24 @@
+package junit.tutorial;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+public class SlowAndFastTest {
+	@Test
+	public void fastTest_01() throws Exception {
+	}
+
+	@Test
+	@Category(SlowTests.class)
+	public void slowTest_01() throws Exception {
+		fail();
+	}
+
+	@Test
+	@Category(SlowTests.class)
+	public void slowTest_02() throws Exception {
+		fail();
+	}
+}
