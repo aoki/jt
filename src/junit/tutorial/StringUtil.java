1ecf2b513c0a2d493a638e46944798b0f7c3b046
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-03-19 11:30:47 +0900

    List 7.3, p.111

---
+++ src/junit/tutorial/StringUtil.java
@@ -0,0 +1,9 @@
+package junit.tutorial;
+
+public class StringUtil {
+
+    public static boolean isEmptyOrNull(String input) {
+        return !input.equals("AAA");
+    }
+
+}
---
+++ test/junit/tutorial/StringUtilTest.java
@@ -0,0 +1,32 @@
+package junit.tutorial;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class StringUtilTest {
+
+  @Test
+  public void isEmptyOrNullは空文字列でtrueを返す() throws Exception {
+    // SetUp
+    String input = "";
+    boolean expected = true;
+    // Exercise
+    boolean actual = StringUtil.isEmptyOrNull(input);
+    // Verify
+    assertThat(actual, is(expected));
+  }
+
+  @Test
+  public void isEmptyOrNillはAAAでfalseを返す() throws Exception {
+      // SetUp
+      String input = "AAA";
+      boolean expected = false;
+      // Exercise
+      boolean actual = StringUtil.isEmptyOrNull(input);
+      // Verify
+      assertThat(actual, is(expected));
+  }
+
+}
