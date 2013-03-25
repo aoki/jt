5013931ccd35ab4570c77c1a54ed00c9b1b52c57
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-03-25 11:59:10 +0900

    List 7.A, p.118

--- test/junit/tutorial/bookstore/BookStoreTestHelper.java
+++ test/junit/tutorial/bookstore/BookStoreTestHelper.java
@@ -3,13 +3,17 @@ package junit.tutorial.bookstore;
 public class BookStoreTestHelper {
 
   public static Book Bookオブジェクトの作成_MartinFlowlerのRefactoring() {
-    Book book = new Book();
-    book.setTitle("Refactoring");
-    book.setPrice(4500);
-    Author author = new Author();
-    author.setFirstName("Martin");
-    author.setLastName("Fowler");
-    book.setAuthor(author);
-    return book;
+    return new Book() {
+      {
+        setTitle("Refactoring");
+        setPrice(4500);
+        setAuthor(new Author() {
+          {
+            setFirstName("Martin");
+            setLastName("Fowler");
+          }
+        });
+      };
+    };
   }
 }
