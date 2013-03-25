c505987f694ceb4b80d38f6d86d04de726c5308b
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-03-25 14:21:54 +0900

    List 8.2, p.122

--- src/junit/tutorial/Registration.java
+++ src/junit/tutorial/Registration.java
@@ -9,4 +9,18 @@ public class Registration {
   public static boolean canRegister(int age) {
     return 18 <= age;
   }
+
+  /**
+   * 優待会員かどうかを返す
+   * @param age 年齢
+   * @param isRegisterMailMagazine メールマガジンに登録している場合にtrue
+   * @param usePastMonth 前月の利用回数
+   * @return 20歳位上であり、メールマガジンに登録してあり、かつ先月の利用回数が1回以上ならtrue
+   */
+  public static boolean isSpecialMember(int age, boolean isRegisterMailMagazine, int usePastMonth) {
+    if (age < 20) return false;
+    if (!isRegisterMailMagazine) return false;
+    if (usePastMonth < 1) return false;
+    return true;
+  }
 }
