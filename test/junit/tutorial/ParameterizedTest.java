c73beff8730e9aad1f8eab060bb6f28edcf08cbc
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-03-26 11:57:46 +0900

    List 8.9, p.130

--- test/junit/tutorial/ParameterizedTest.java
+++ test/junit/tutorial/ParameterizedTest.java
@@ -12,12 +12,22 @@ public class ParameterizedTest {
   @DataPoint
   public static int INT_PARAN_2 = 4;
 
+  @DataPoint
+  public static String STRING_PARAM_1 = "Hello";
+  @DataPoint
+  public static String STRING_PARAM_2 = "World";
+
   public ParameterizedTest() {
     System.out.println("初期化");
   }
 
   @Theory
-  public void 引数を持つテストメソッド(int param) throws Exception {
-    System.out.println("引数を持つテストメソッド(" + param +")");
+  public void 引数がint型のテストメソッド(int param) throws Exception {
+    System.out.println("引数がint型のテストメソッド(" + param + ")");
+  }
+
+  @Theory
+  public void 引数がString型のテストメソッド(String param) throws Exception {
+    System.out.println("引数がString型のテストメソッド(" + param + ")");
   }
 }
