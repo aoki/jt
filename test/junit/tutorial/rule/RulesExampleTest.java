50004e2f413435b3f060523c28bd1a92acf69be8
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-03-29 11:37:54 +0900

    List 9.2, p.143

---
+++ test/junit/tutorial/rule/RulesExampleTest.java
@@ -0,0 +1,18 @@
+package junit.tutorial.rule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TestName;
+
+public class RulesExampleTest {
+  @Rule
+  public TestName testName = new TestName();
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
+  @Test
+  public void test() {
+  }
+
+}
