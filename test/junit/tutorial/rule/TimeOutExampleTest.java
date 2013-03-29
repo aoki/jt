b9710ae61d5361b2dc20ef7420cda9bcd9f5ff04
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-03-29 11:34:53 +0900

    List 9.1, p.142

---
+++ test/junit/tutorial/rule/TimeOutExampleTest.java
@@ -0,0 +1,22 @@
+package junit.tutorial.rule;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+public class TimeOutExampleTest {
+  @Rule
+  public Timeout timeout = new Timeout(100);
+
+  @Test
+  public void 長い時間がかかるかもしれないテスト() throws Exception {
+    try {
+      java.util.concurrent.TimeUnit.MILLISECONDS.sleep(10);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
