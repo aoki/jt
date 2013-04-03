90823b841ad9e18080d00766abe0163b5e445fea
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-04-03 11:48:35 +0900

    List 9.11, p.151

--- test/junit/tutorial/rule/ExpectedExceptionExampleTest.java
+++ test/junit/tutorial/rule/ExpectedExceptionExampleTest.java
@@ -4,7 +4,9 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class ExpectedExceptionExampleTest {
 
@@ -24,6 +26,16 @@ public class ExpectedExceptionExampleTest {
   }
 
   private void throwNewIllegalArgumentException() {
+    throw new IllegalArgumentException("argument is null.");
+  }
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
+  @Test
+  public void 例外の発生とメッセージを検証するテスト() throws Exception {
+    expectedException.expect(IllegalArgumentException.class);
+    expectedException.expectMessage("argument is null.");
     throw new IllegalArgumentException("argument is null.");
   }
 
