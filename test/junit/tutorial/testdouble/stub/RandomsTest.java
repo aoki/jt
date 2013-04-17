e5e58633b4d17a71c186c53ec076aabc9d06b654
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-04-17 12:24:23 +0900

    List 11.15, p.184

---
+++ test/junit/tutorial/testdouble/stub/RandomsTest.java
@@ -0,0 +1,45 @@
+package junit.tutorial.testdouble.stub;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class RandomsTest {
+
+  @Test
+  public void choiceでAを返す() throws Exception {
+    List<String> options = new ArrayList<String>();
+    options.add("A");
+    options.add("B");
+    Randoms sut = new Randoms();
+    // スタブの設定
+    sut.generator = new RandomNumberGenerator() {
+      @Override
+      public int nextInt() {
+        return 0;
+      }
+    };
+    assertThat(sut.choice(options), is("A"));
+  }
+
+  @Test
+  public void choiceでBを返す() throws Exception {
+    List<String> options = new ArrayList<String>();
+    options.add("A");
+    options.add("B");
+    Randoms sut = new Randoms();
+    // スタブの設定
+    sut.generator = new RandomNumberGenerator() {
+      @Override
+      public int nextInt() {
+        return 1;
+      }
+    };
+    assertThat(sut.choice(options), is("B"));
+  }
+
+}
