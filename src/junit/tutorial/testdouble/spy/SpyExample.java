6baa4c11f3ecd8e16a7ead36cf8de36b6f0b73d0
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-04-22 11:40:20 +0900

    List 11.20, p.189

---
+++ src/junit/tutorial/testdouble/spy/SpyExample.java
@@ -0,0 +1,13 @@
+package junit.tutorial.testdouble.spy;
+
+import java.util.logging.Logger;
+
+public class SpyExample {
+
+  Logger logger = Logger.getLogger(SpyExample.class.getName());
+
+  public void doSomething() {
+    logger.info("doSomething");
+  }
+
+}
