b617014614f3cf004011bfd5c4183896c6d9efc9
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2013-05-09 12:02:54 +0900

    List 11.25, p.193

--- test/junit/tutorial/testdouble/mockito/MockitoTest.java
+++ test/junit/tutorial/testdouble/mockito/MockitoTest.java
@@ -28,4 +28,12 @@ public class MockitoTest {
     assertThat(stub.get(0), is("Hello"));   // 検証
   }
 
+  @Test(expected = IndexOutOfBoundsException.class)
+  public void 例外を送出するスタブメソッド() {
+    List<String> stub = mock(List.class);
+    when(stub.get(0)).thenReturn("Hello");
+    when(stub.get(1)).thenReturn("World");
+    when(stub.get(2)).thenThrow(new IndexOutOfBoundsException());
+    stub.get(2); // 例外が創出される
+  }
 }
